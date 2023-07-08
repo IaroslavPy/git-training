c18fd616b6cf1b396d72698d9dd1328673b1ab14
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-08 16:23:51 +0300

    Add Develop created 2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
-        System.out.println("Develop created 1!");
+        System.out.println("Develop created 2!");
         int a = 8;
         int b = 2;
         int c = add(a, b);
